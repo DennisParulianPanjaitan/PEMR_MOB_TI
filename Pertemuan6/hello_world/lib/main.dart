87cd7843cfdfdf1be7f9f0444d2cda23ed1c6bff
DennisParulianPanjaitan <drekodraco1@gmail.com>
2024-10-26 19:14:28 +0700

    Pertemuan 6 Praktikum 3

--- Pertemuan6/hello_world/lib/main.dart
+++ Pertemuan6/hello_world/lib/main.dart
@@ -60,6 +60,19 @@ class MyApp extends StatelessWidget {
       ],
     );
 
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: const Text(
+        'Carilah teks di internet yang sesuai '
+        'dengan foto atau tempat wisata yang ingin '
+        'Anda tampilkan. '
+        'Tambahkan nama dan NIM Anda sebagai '
+        'identitas hasil pekerjaan Anda. '
+        'Selamat mengerjakan 🙂.',
+        softWrap: true,
+      ),
+    );
+
     return MaterialApp(
       title: 'Flutter layout: Nama dan NIM Anda',
       home: Scaffold(
@@ -70,9 +83,7 @@ class MyApp extends StatelessWidget {
           children: [
             titleSection, // Display the title section
             buttonSection,
-            const Center(
-              child: Text('Hello World'),
-            ),
+            textSection,
           ],
         ),
       ),
